80850be0d856a42f7d510e2a7aacb35b65baac4b
Lorenzo Ruiz <lars.bs@hotmail.com>
2018-08-27 10:40:43 +0200

    Add darken function

--- packages/style-vars/src/colors.js
+++ packages/style-vars/src/colors.js
@@ -9,3 +9,8 @@ module.exports.lighten = function lighten(color, amount) {
 module.exports.fade = function fade(color, amount) {
   return tc(color).setAlpha(amount / 100).toString();
 }
+
+
+module.exports.darken = function darken(color, amount) {
+  return tc(color).darken(amount).toString();
+}
