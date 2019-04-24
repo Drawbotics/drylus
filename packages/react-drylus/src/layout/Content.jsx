4fdfaddfa37bd5a79c5ecf1308dfdd440ddeed7f
nicmosc <nmoscholios@gmail.com>
2019-04-24 17:19:25 +0200

    add proptypes

--- packages/react-drylus/src/layout/Content.jsx
+++ packages/react-drylus/src/layout/Content.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { css, cx } from 'emotion';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import sv from '@drawbotics/style-vars';
 
 
@@ -31,6 +31,15 @@ const Content = ({ children, fullHeight, fullWidth }) => {
       </div>
     </div>
   );
+};
+
+
+Content.propTypes = {
+  children: PropTypes.node.isRequired,
+
+  fullHeight: PropTypes.bool,
+
+  fullWidth: PropTypes.bool,
 };
 
 
