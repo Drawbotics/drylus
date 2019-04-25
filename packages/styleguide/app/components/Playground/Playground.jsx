25e8f5c5481ed19f5ac1c95d59b6707e29c7a22f
nicmosc <nmoscholios@gmail.com>
2019-04-25 18:18:33 +0200

    mode switcher draft

--- packages/styleguide/app/components/Playground/Playground.jsx
+++ packages/styleguide/app/components/Playground/Playground.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-// import ReactDOMServer from 'react-dom/server';
+import ReactDOMServer from 'react-dom/server';
 import ReactElementToString from 'react-element-to-jsx-string';
 import omit from 'lodash/omit';
 import { css } from 'emotion';
@@ -8,29 +8,47 @@ import sv from '@drawbotics/style-vars';
 import CodeBox from './CodeBox';
 import Preview from './Preview';
 import PropsTable from './PropsTable';
+import ModeSwitcher from './ModeSwitcher';
 
 
 const styles = {
   playground: css`
     padding: ${sv.basePadding};
     border: 1px solid ${sv.neutralDark};
+    position: relative;
   `,
 };
 
 
+function getMarkupForMode(mode, component) {
+  const generatedHTMLString = ReactDOMServer.renderToStaticMarkup(component);
+  const generatedJSXString = ReactElementToString(component);
+  switch (mode) {
+    case 'vanilla':
+      return generatedHTMLString;
+    case 'react':
+      return generatedJSXString;
+    default:
+      console.warn('Render mode not supported');
+      return '';
+  }
+}
+
+
 const Playground = ({ component: Component, initialProps }) => {
   const [props, setProps] = useState({});
+  const [activeMode, setMode] = useState('react');
 
   const generatedComponent = <Component {...initialProps} {...props} />;
-  // const generatedHTMLString = ReactDOMServer.renderToStaticMarkup(generatedComponent);
-  const generatedJSXString = ReactElementToString(generatedComponent);
+  const generatedMarkup = getMarkupForMode(activeMode, generatedComponent);
   return (
     <div className={styles.playground}>
       <h3>You are currently within the playground</h3>
+      <ModeSwitcher modes={['react', 'vanilla']} activeMode={activeMode} onChange={setMode} />
       <Preview component={Component}>
         {generatedComponent}
       </Preview>
-      <CodeBox>{generatedJSXString}</CodeBox>
+      <CodeBox>{generatedMarkup}</CodeBox>
       <PropsTable component={Component} activeProps={props} onChange={(v, n) => v === '_empty' ? setProps(omit(props, n)) : setProps({ ...props, [n]: v })} />
     </div>
   );
