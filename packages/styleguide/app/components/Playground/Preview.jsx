733cb4ce062d36a6f62a49f92451c7321452374d
nicmosc <nmoscholios@gmail.com>
2019-04-26 17:59:27 +0200

    wip

--- packages/styleguide/app/components/Playground/Preview.jsx
+++ packages/styleguide/app/components/Playground/Preview.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { LiveProvider, LivePreview } from 'react-live';
 import { css } from 'emotion';
 import sv from '@drawbotics/style-vars';
 
@@ -12,22 +11,6 @@ const styles = {
     margin: ${sv.baseMarginSmall};
   `,
 };
-
-
-// const _Preview = ({ children, component }) => {
-//   const componentName = component.displayName || component.name;
-//   if (! componentName) {
-//     console.warn('Component has no name, won\'t render');
-//     return null;
-//   }
-//   return (
-//     <div style={{ marginTop: '40px' }}>
-//       <LiveProvider code={children} scope={{ [componentName]: component }}>
-//         <LivePreview />
-//       </LiveProvider>
-//     </div>
-//   );
-// };
 
 
 const Preview = ({ children, raw }) => {
