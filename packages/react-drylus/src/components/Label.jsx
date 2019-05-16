d255af87bbb8e34abaece96906bcb95653fdd549
nicmosc <nmoscholios@gmail.com>
2019-05-16 10:50:54 +0200

    get started with tables

---
+++ packages/react-drylus/src/components/Label.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Label = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+
+export default Label;
---
+++ packages/react-drylus/src/components/Table.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Table = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+
+export default Table;
