2b70384d47807ecfdd0c242d587397e01c761e4a
nicmosc <nmoscholios@gmail.com>
2019-05-29 18:17:53 +0200

    finish tooltip

--- packages/react-drylus/src/components/index.js
+++ packages/react-drylus/src/components/index.js
@@ -18,4 +18,4 @@ export TabNavigation from './TabNavigation';
 export ListTile from './ListTile';
 export Avatar from './Avatar';
 export Dot from './Dot';
-export Tooltip from './Tooltip';
+export Tooltip, { TooltipSides } from './Tooltip';
