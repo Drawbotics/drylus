aa951f53d690988eb0ab98f0ce8543f6053cc007
nicmosc <nmoscholios@gmail.com>
2019-07-10 16:30:44 +0200

    set up alert architecture

---
+++ packages/react-drylus/src/components/AlertsProvider.jsx
@@ -0,0 +1,114 @@
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import { css } from 'emotion';
+import sv from '@drawbotics/style-vars';
+import PropTypes from 'prop-types';
+import v4 from 'uuid/v4';
+
+import { Categories } from '../base';
+
+
+const styles = {
+  provider: css`
+    position: fixed;
+    z-index: 9999;
+    bottom: 0;
+    left: 0;
+  `,
+  root: css`
+    padding: ${sv.defaultPadding};
+    background: red;
+  `,
+};
+
+
+const Context = React.createContext();
+
+
+export const Alert = ({
+  text,
+  category,
+  onClickDismiss,
+}) => {
+  return (
+    <div className={styles.root}>
+      {text}
+    </div>
+  );
+};
+
+
+Alert.propTypes = {
+  /** Text shown within the alert */
+  text: PropTypes.string.isRequired,
+
+  category: PropTypes.oneOf([
+    Categories.DANGER,
+    Categories.SUCCESS,
+    Categories.INFO,
+    Categories.WARNING,
+  ]).isRequired,
+
+  /** Triggered when the dismiss button is clicked */
+  onClickDismiss: PropTypes.func,
+
+  /** If you need to manually hide the alert, you can pass your own ID to call hide */
+  id: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string,
+  ]),
+};
+
+
+const AlertsProvider = ({ children }) => {
+  const [outletElement, setOutletElement] = useState(null);
+  const [alerts, setAlerts] = React.useState([]);
+
+  const show = (alertProps) => {
+    const id = v4();
+    setAlerts([ ...alerts, { id, ...alertProps } ]);
+  };
+
+  const hide = (id) => {
+    setAlerts(alerts.filter((alert) => alert.id !== id));
+  };
+
+  useEffect(() => {
+    if ( ! document.getElementById('alerts-outlet')) {
+      const alertsOutlet = document.createElement('div');
+      alertsOutlet.id = 'alerts-outlet';
+      document.body.appendChild(alertsOutlet);
+      setOutletElement(alertsOutlet);
+    }
+    else {
+      setOutletElement(document.getElementById('alerts-outlet'));
+    }
+
+    return () => {
+      if (outletElement) {
+        document.body.removeChild(outletElement);
+      }
+    };
+  }, []);
+
+  if (! outletElement) return null;
+
+  return (
+    <Context.Provider value={{ show, hide }}>
+      {children}
+      {ReactDOM.createPortal(
+        <div className={styles.provider}>
+          {alerts.map((props) => (
+            <Alert key={props.id} onClickDismiss={hide} {...props} />
+          ))}
+        </div>,
+        document.getElementById('alerts-outlet'),
+      )}
+    </Context.Provider>
+  );
+};
+
+
+export default AlertsProvider;
+
+export const useAlert = () => React.useContext(Context);
