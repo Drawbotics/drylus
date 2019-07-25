2699f5499497cc542ea2eb482724de245f4a9d81
nicmosc <nmoscholios@gmail.com>
2019-07-25 15:43:42 +0200

    remove unused behaviour in searchInput

--- packages/vanilla-drylus/src/components/searchInput.js
+++ packages/vanilla-drylus/src/components/searchInput.js
@@ -3,22 +3,11 @@ const searchInput = () => {
 
   const handleClickSearch = (input) => input.focus();
 
-  const handleFocusInput = (list) => {
-    list.classList.add('Drylus-SearchInput__visible');
-  };
-
-  const handleBlurInput = (list) => {
-    list.classList.remove('Drylus-SearchInput__visible');
-  };
-
   Array.from(roots).forEach((root) => {
     const input = root.getElementsByClassName('Drylus-Input__input')[0];
     const button = root.getElementsByClassName('Drylus-Button__root')[0];
-    const list = root.getElementsByClassName('Drylus-SearchInput__list')[0];
 
     button.addEventListener('click', () => handleClickSearch(input));
-    input.addEventListener('focus', () => handleFocusInput(list));
-    input.addEventListener('blur', () => handleBlurInput(list));
   });
 };
 
