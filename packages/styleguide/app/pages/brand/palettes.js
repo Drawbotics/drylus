bdfead14f5ebbd46753dbb6ab9df965f32e40c04
nicmosc <nmoscholios@gmail.com>
2019-08-13 13:43:27 +0200

    finish all color palettes

--- packages/styleguide/app/pages/brand/palettes.js
+++ packages/styleguide/app/pages/brand/palettes.js
@@ -44,6 +44,7 @@ export const greys = {
   ],
 };
 
+
 export const neutrals = {
   title: 'Neutrals',
   background: sv.neutralDarker,
@@ -73,6 +74,7 @@ export const neutrals = {
   ],
 };
 
+
 export const azures = {
   title: 'Azures',
   background: sv.azureDark,
@@ -88,6 +90,139 @@ export const azures = {
     },
     {
       name: 'azureDark',
+    },
+  ],
+};
+
+
+export const brands = {
+  title: 'Brands',
+  background: sv.brand,
+  description: 'Used as a highlight when no particular status is required e.g. generic CTAs',
+  colors: [
+    {
+      name: 'brandLighter',
+      light: true,
+    },
+    {
+      name: 'brandLight',
+      light: true,
+    },
+    {
+      name: 'brand',
+    },
+    {
+      name: 'brandDark',
+    },
+  ],
+};
+
+
+export const blues = {
+  title: 'Blues',
+  background: sv.blue,
+  description: 'These are used for neutral information, such as buttons confirming a neutral action (e.g. closing a modal), links, etc.',
+  colors: [
+    {
+      name: 'blueLighter',
+      light: true,
+    },
+    {
+      name: 'blueLight',
+      light: true,
+    },
+    {
+      name: 'blue',
+    },
+    {
+      name: 'blueDark',
+    },
+  ],
+};
+
+
+export const reds = {
+  title: 'Reds',
+  background: sv.red,
+  description: 'These are for destructive actions and for errors',
+  colors: [
+    {
+      name: 'redLighter',
+      light: true,
+    },
+    {
+      name: 'redLight',
+      light: true,
+    },
+    {
+      name: 'red',
+    },
+    {
+      name: 'redDark',
+    },
+  ],
+};
+
+
+export const oranges = {
+  title: 'Oranges',
+  background: sv.orange,
+  description: 'Used for actions or elements on the page which the user should be warned/be careful about',
+  colors: [
+    {
+      name: 'orangeLighter',
+      light: true,
+    },
+    {
+      name: 'orangeLight',
+      light: true,
+    },
+    {
+      name: 'orange',
+    },
+    {
+      name: 'orangeDark',
+    },
+  ],
+};
+
+
+export const greens = {
+  title: 'Green',
+  background: sv.green,
+  description: 'These are for constructive actions, or success events, essentially showing something "good" that happened or is displayed',
+  colors: [
+    {
+      name: 'greenLighter',
+      light: true,
+    },
+    {
+      name: 'greenLight',
+      light: true,
+    },
+    {
+      name: 'green',
+    },
+    {
+      name: 'greenDark',
+    },
+  ],
+};
+
+
+export const socials = {
+  title: 'Socials',
+  background: sv.facebookColor,
+  description: 'If you need to reference any of the social networks, use their brand colors',
+  colors: [
+    {
+      name: 'facebookColor',
+    },
+    {
+      name: 'linkedinColor',
+    },
+    {
+      name: 'twitterColor',
     },
   ],
 };
