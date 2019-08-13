cf9626a9ed27af51c58252e81e83e88b581d79b5
nicmosc <nmoscholios@gmail.com>
2019-08-13 11:02:17 +0200

    add tooltip description

---
+++ packages/styleguide/app/pages/brand/palettes.js
@@ -0,0 +1,93 @@
+import sv from '@drawbotics/drylus-style-vars';
+
+
+export const greys = {
+  title: 'Greys',
+  background: sv.grey800,
+  description: 'Rarely used and are mostly for utility purpses.',
+  colors: [
+    {
+      name: 'white',
+      light: true,
+    },
+    {
+      name: 'grey50',
+      light: true,
+    },
+    {
+      name: 'grey100',
+      light: true,
+    },
+    {
+      name: 'grey300',
+      light: true,
+    },
+    {
+      name: 'grey400',
+      light: true,
+    },
+    {
+      name: 'grey500',
+    },
+    {
+      name: 'grey600',
+    },
+    {
+      name: 'grey700',
+    },
+    {
+      name: 'grey800',
+    },
+    {
+      name: 'grey900',
+    },
+  ],
+};
+
+export const neutrals = {
+  title: 'Neutrals',
+  background: sv.neutralDarker,
+  description: 'Neutrals determine backgrounds and other secondary colorations.',
+  colors: [
+    {
+      name: 'neutralLighter',
+      light: true,
+    },
+    {
+      name: 'neutralLight',
+      light: true,
+    },
+    {
+      name: 'neutral',
+      light: true,
+    },
+    {
+      name: 'neutralDark',
+    },
+    {
+      name: 'neutralDarker',
+    },
+    {
+      name: 'neutralDarkest',
+    },
+  ],
+};
+
+export const azures = {
+  title: 'Azures',
+  background: sv.azureDark,
+  description: 'Azures are used only in forms (borders, backgrounds, etc).',
+  colors: [
+    {
+      name: 'azureLight',
+      light: true,
+    },
+    {
+      name: 'azure',
+      light: true,
+    },
+    {
+      name: 'azureDark',
+    },
+  ],
+};
