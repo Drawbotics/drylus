2ff5f72bbb3af7861f8a6193ea4a02326f1d1e14
nicmosc <nmoscholios@gmail.com>
2019-09-13 13:52:00 +0200

    add fullscreen mode to preview

--- packages/styleguide/app/components/Playground/Preview.jsx
+++ packages/styleguide/app/components/Playground/Preview.jsx
@@ -1,19 +1,54 @@
-import React, { Fragment } from 'react';
-import { css } from 'emotion';
+import React, { Fragment, useState } from 'react';
+import { css, cx } from 'emotion';
 import sv from '@drawbotics/drylus-style-vars';
+import { TextLink, Icon } from '@drawbotics/react-drylus';
 
 
 const styles = {
   preview: css`
+    position: relative;
     padding: ${sv.defaultPadding};
     background: ${sv.white};
+
+    &:hover {
+      [data-element="full-screen-toggle"] {
+        opacity: 1;
+        pointer-events: auto;
+      }
+    }
+  `,
+  fullScreen: css`
+    position: fixed;
+    top: ${sv.marginExtraSmall};
+    left: ${sv.marginExtraSmall};
+    z-index: 9999;
+    min-height: calc(100vh - ${sv.marginSmall});
+    min-width: calc(100vw - ${sv.marginSmall});
+    overflow: hidden;
+    border-radius: ${sv.defaultBorderRadius};
+  `,
+  fullScreenToggle: css`
+    position: absolute;
+    bottom: ${sv.marginExtraSmall};
+    right: ${sv.marginExtraSmall};
+    pointer-events: none;
+    opacity: 0;
   `,
 };
 
 
 const Preview = ({ children, raw }) => {
+  const [ fullScreen, setFullScreen ] = useState(false);
+  
   return (
-    <div className={styles.preview}>
+    <div className={cx(styles.preview, { [styles.fullScreen]: fullScreen })}>
+      <div className={styles.fullScreenToggle} data-element="full-screen-toggle">
+        <div onClick={() => setFullScreen(! fullScreen)}>
+          <TextLink>
+            <Icon name={fullScreen ? 'minimize' : 'maximize'} />
+          </TextLink>
+        </div>
+      </div>
       {do{
         if (raw) {
           <div dangerouslySetInnerHTML={{ __html: children }} />
