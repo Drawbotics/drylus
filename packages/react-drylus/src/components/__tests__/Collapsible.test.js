314872307c4ba23fe26dfaee8936c907ace563a8
nicmosc <nmoscholios@gmail.com>
2019-10-04 10:39:39 +0200

    add tests for collapsible

--- packages/react-drylus/src/components/__tests__/Collapsible.test.js
+++ packages/react-drylus/src/components/__tests__/Collapsible.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { create } from 'react-test-renderer';
 
 import Collapsible from '../Collapsible';
 
@@ -7,10 +7,35 @@ import Collapsible from '../Collapsible';
 describe('Collapsible', () => {
   describe('matches snapshot when', () => {
     it('is not open', () => {
-      const tree = renderer.create(
+      const tree = create(
         <Collapsible title="Title" isOpen={false}>Collapsible content</Collapsible>
       ).toJSON();
       expect(tree).toMatchSnapshot();
     });
+
+    it('it is open', () => {
+      const tree = create(
+        <Collapsible title="Title" isOpen={true}>Collapsible content</Collapsible>
+      ).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+  describe('opens when clicked', () => {
+    let open = false;
+
+    expect(open).toBeFalsy();
+
+    const component = create(
+      <Collapsible
+        title="Title"
+        isOpen={false}
+        onClick={() => open = true}>
+        Collapsible content
+      </Collapsible>
+    );
+
+    component.root.props.onClick();
+    
+    expect(open).toBeTruthy();
   });
 });
