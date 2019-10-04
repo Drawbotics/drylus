d53ef0f70f540b5e6e894ca8397a909c03164913
nicmosc <nmoscholios@gmail.com>
2019-10-04 11:09:33 +0200

    configure emotion to show css instead of just classnames

--- packages/react-drylus/jest.setup.js
+++ packages/react-drylus/jest.setup.js
@@ -1,3 +1,15 @@
+import { createSerializer } from 'jest-emotion';
+
+
+// Set up for jest to display css values in snapshot
+expect.addSnapshotSerializer(
+  createSerializer({
+    classNameReplacer: (className, index) => className,
+  })
+);
+
+
+// Create missing function
 window.URL.createObjectURL = function() {};
 
 
