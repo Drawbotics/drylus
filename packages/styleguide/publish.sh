d9d14d69bc432b31c07f928d48f504e38537fc2b
nicmosc <nmoscholios@gmail.com>
2019-10-05 18:53:39 +0200

    fix publish script

--- packages/styleguide/publish.sh
+++ packages/styleguide/publish.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-git config --global user.email "nick@drawbotics.com"
-git config --global user.name "Nicolaos Moscholios"
+git config --global user.email "nicmosc@users.noreply.github.com"
+git config --global user.name "nicmosc"
 
 git add ../../docs
 
 git commit -m "New styleguide build" --no-verify --allow-empty
 
-git push -u origin HEAD
+git push ${GITHUB_REF:11}:master
