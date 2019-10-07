f1a46e2ae7359e4e66f102d868f88e1d7772d7e0
nicmosc <nicmosc@users.noreply.github.com>
2019-10-07 11:02:45 +0200

    use npx

--- release.sh
+++ release.sh
@@ -5,7 +5,7 @@ export PATH=$(npm bin):$PATH
 VERSION=`auto version`
 
 if [ ! -z "$VERSION" ]; then
-  auto changelog
-  lerna publish --yes $VERSION -m '%v [skip ci]'
-  auto release
+  npx auto changelog
+  npx lerna publish --yes $VERSION -m '%v [skip ci]'
+  npx auto release
 fi
