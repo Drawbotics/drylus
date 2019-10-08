b9dd1eae131049f0cc0e3dd4ac05e394579e755e
nicmosc <nicmosc@users.noreply.github.com>
2019-10-08 15:29:08 +0200

    remove from-package option from lerna publish

--- release.sh
+++ release.sh
@@ -6,5 +6,5 @@ VERSION=`npx auto version`
 
 if [ ! -z "$VERSION" ]; then
   npx auto changelog
-  npx lerna publish from-package --yes --no-verify-access $VERSION -m '%v [skip ci]'
+  npx lerna publish --yes --no-verify-access $VERSION -m '%v [skip ci]'
 fi
