f854e7a127896722a0527d0701a1f7d6ae0fe22d
nicmosc <nicmosc@users.noreply.github.com>
2019-10-24 10:12:32 +0200

    add title tests

---
+++ packages/react-drylus/src/components/__tests__/Title.test.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { create } from 'react-test-renderer';
+
+import Title from '../Title';
+
+
+describe('Title', () => {
+  describe('matches snapshot when', () => {
+    it('has a h1 size', () => {
+      const tree = create(
+        <Title size={1}>Title content</Title>
+      ).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it('has a h2 size', () => {
+      const tree = create(
+        <Title size={2}>Title content</Title>
+      ).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it('has a h3 size', () => {
+      const tree = create(
+        <Title size={3}>Title content</Title>
+      ).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it('has a h4 size', () => {
+      const tree = create(
+        <Title size={4}>Title content</Title>
+      ).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it('does not have margins', () => {
+      const tree = create(
+        <Title noMargin>Title content</Title>
+      ).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it('has a node as children', () => {
+      const tree = create(
+        <Title size={2}>
+          <span style={{ fontWeight: 500 }}>Some </span> content
+        </Title>
+      ).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+});
