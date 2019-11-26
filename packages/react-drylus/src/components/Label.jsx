9de11ee0f3fdf5632e3d02f9ffb82f1e4d573521
nicmosc <nicmosc@users.noreply.github.com>
2019-11-26 16:41:57 +0100

    add isPlaceholder prop to label

--- packages/react-drylus/src/components/Label.jsx
+++ packages/react-drylus/src/components/Label.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import { css, cx } from 'emotion';
+import { css, cx, keyframes } from 'emotion';
 import PropTypes from 'prop-types';
 import sv from '@drawbotics/drylus-style-vars';
 
 
+const shimmer = keyframes`
+  0% {
+    background-position: -1200px 0;
+  }
+  100% {
+    background-position: 1200px 0;
+  }
+`;
+
+
+
 const styles = {
   root: css`
+    display: inline-block;
     color: ${sv.colorTertiary};
     text-transform: uppercase;
     font-weight: 500;
@@ -19,12 +31,45 @@ const styles = {
     overflow: hidden;
     text-overflow: ellipsis;
   `,
+  withPlaceholderOverlay: css`
+    position: relative;
+
+    &::after {
+      content: ' ';
+      position: absolute;
+      z-index: 9;
+      height: 100%;
+      width: 100%;
+      top: 0;
+      left: 0;
+      background: ${sv.neutralLight};
+      border-radius: ${sv.defaultBorderRadius};
+      overflow: hidden;
+      background: linear-gradient(to right,
+        ${sv.neutralLight} 8%,
+        ${sv.neutralLighter} 18%,
+        ${sv.neutralLight} 33%
+      );
+      background-size: 1200px 100%;
+      animation: ${shimmer} 2s forwards infinite linear;
+    }
+  `,
 };
 
 
-const Label = ({ children, ellipsized, style }) => {
+const Label = ({
+  children,
+  ellipsized,
+  style,
+  isPlaceholder,
+}) => {
   return (
-    <div style={style} className={cx(styles.root, { [styles.ellipsized]: ellipsized })}>
+    <div
+      style={style}
+      className={cx(styles.root, {
+        [styles.ellipsized]: ellipsized,
+        [styles.withPlaceholderOverlay]: isPlaceholder,
+      })}>
       {children}
     </div>
   );
@@ -40,6 +85,9 @@ Label.propTypes = {
 
   /** Used for style overrides */
   style: PropTypes.object,
+
+  /** If true, a loading overlay is displayed on top of the component */
+  isPlaceholder: PropTypes.bool,
 };
 
 
