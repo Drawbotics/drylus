0ce97d907f2099cf34f07759f77eb606d82ebee4
nicmosc <nicmosc@users.noreply.github.com>
2019-11-27 10:34:49 +0100

    add onChange test for radio group

--- packages/react-drylus/src/forms/RadioGroup.jsx
+++ packages/react-drylus/src/forms/RadioGroup.jsx
@@ -155,11 +155,12 @@ const styles = {
       height: ${sv.marginSmall};
       width: ${sv.marginSmall};
     }
-    [data-element="icon"] {
+
+    [data-element="icon"], [data-element="locked-icon"] {
       line-height: calc(${sv.marginSmall} + 1px);
 
        > i {
-         font-size: 0.7rem;
+         font-size: 0.7rem !important;
        }
     }
   `,
