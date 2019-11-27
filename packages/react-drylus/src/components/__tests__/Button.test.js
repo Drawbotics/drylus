0dda56b86b46f294b785ee011d3820bc631fa4c1
nicmosc <nicmosc@users.noreply.github.com>
2019-11-27 14:46:48 +0100

    add Button snapshot tests

---
+++ packages/react-drylus/src/components/__tests__/Button.test.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { create } from 'react-test-renderer';
+
+import Button from '../Button';
+import {
+  Sizes,
+  Tiers,
+  Categories,
+} from '../../base';
+import Icon from '../Icon';
+
+
+describe('Button', () => {
+  describe('matches snapshot when', () => {
+    it('is basic', () => {
+      const tree = create(
+        <Button>
+          Some content
+        </Button>
+      ).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it('has a category', () => {
+      const tree = create(
+        <Button category={Categories.WARNING}>
+          Some content
+        </Button>
+      ).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it('has a tier', () => {
+      const tree = create(
+        <Button tier={Tiers.SECONDARY}>
+          Some content
+        </Button>
+      ).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it('is small', () => {
+      const tree = create(
+        <Button size={Sizes.SMALL}>
+          Some content
+        </Button>
+      ).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it('has a leading component', () => {
+      const tree = create(
+        <Button leading={<Icon name="check" />}>
+          Some content
+        </Button>
+      ).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it('has a trailing component', () => {
+      const tree = create(
+        <Button trailing={<Icon name="check" />}>
+          Some content
+        </Button>
+      ).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+});
