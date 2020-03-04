99f6de1783059e9d24e6582f69fa2982d41a1af2
nicmosc <nicmosc@users.noreply.github.com>
2020-03-04 12:33:24 +0100

    bump pre-release version

--- pre-release.sh
+++ pre-release.sh
@@ -2,6 +2,10 @@
 
 export PATH=$(npm bin):$PATH
 
-# npx lerna version prerelease --no-commit-hooks --no-changelog --no-git-tag-version --no-push
+npx lerna version prerelease --no-commit-hooks --no-changelog --no-git-tag-version --no-push
+
+git add .
+
+git commit -m "bump pre-release version"
 
 npx lerna publish from-package --ignore-prepublish --no-git-reset --pre-dist-tag alpha
