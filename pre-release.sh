b120e2374a73abd0bf322d3d58b7b307ed0ccdae
nicmosc <nicmosc@users.noreply.github.com>
2020-03-04 12:31:47 +0100

    edit pre-release script

--- pre-release.sh
+++ pre-release.sh
@@ -2,6 +2,6 @@
 
 export PATH=$(npm bin):$PATH
 
-npx lerna version preminor --no-commit-hooks --no-changelog --no-git-tag-version --no-push
+# npx lerna version prerelease --no-commit-hooks --no-changelog --no-git-tag-version --no-push
 
-npx lerna publish from-package --ignore-prepublish --yes
+npx lerna publish from-package --ignore-prepublish --no-git-reset --pre-dist-tag alpha
