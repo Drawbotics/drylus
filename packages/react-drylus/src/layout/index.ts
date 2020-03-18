383e0ab4643d3879dfbc9c4aeb09bff9de52364d
nicmosc <nicmosc@users.noreply.github.com>
2020-03-18 15:34:56 +0100

    convert all layout

--- packages/react-drylus/src/layout/index.ts
+++ packages/react-drylus/src/layout/index.ts
@@ -2,5 +2,5 @@ export * from './Layout';
 export * from './Flex';
 export * from './Margin';
 export * from './Padding';
-// export Grid, { GridItem } from './Grid';
+export * from './Grid';
 export * from './ListTile';
