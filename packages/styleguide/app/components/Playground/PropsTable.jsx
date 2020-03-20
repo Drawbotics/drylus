3fbc802f00c1182c2d2f653ffe7de1f239b66ab0
LeopoldArkham <leopold.arkham@gmail.com>
2020-03-20 12:57:58 +0100

    Display enum values

--- packages/styleguide/app/components/Playground/PropsTable.jsx
+++ packages/styleguide/app/components/Playground/PropsTable.jsx
@@ -36,8 +36,22 @@ function _getProps(Component) {
   return description ?? {};
 }
 
+function _hasDescription(prop) {
+  return prop?.description?.text !== '' || prop?.description?.tag != null;
+}
+
+function _isEnum(prop) {
+  return prop?.type?.type === 'enum';
+}
+
+function capitalizeFirst(text) {
+  if (typeof text !== 'string' || text === '') return text;
+  return text[0].toUpperCase() + text.slice(1);
+}
+
 const PropsTable = ({ component, onChange, activeProps, enums }) => {
   const props = _getProps(component);
+
   return (
     <div className={styles.table}>
       <Table>
@@ -52,74 +66,91 @@ const PropsTable = ({ component, onChange, activeProps, enums }) => {
         <TBody>
           {Object.keys(props)
             .sort()
-            .map((key) => (
-              <TRow key={key}>
-                <TCell>{key}</TCell>
-                <TCell>
-                  {do {
-                    if (props[key].type.value) {
-                      <Tooltip
-                        content={<PropsInfo props={props[key].type.value} />}
-                        side={Position.RIGHT}>
-                        <Flex justify={FlexJustify.START}>
-                          <FlexItem>{props[key].type.name}</FlexItem>
-                          <FlexItem>
-                            <Margin size={{ left: Size.EXTRA_SMALL }}>
-                              <span style={{ color: sv.colorSecondary }}>
-                                <Icon name="info" />
-                              </span>
-                            </Margin>
-                          </FlexItem>
-                        </Flex>
-                      </Tooltip>;
-                    } else {
-                      props[key].type.name;
-                    }
-                  }}
-                </TCell>
-                <TCell>{/* {props[key].defaultValue?.value.replace(/'/g, '') || null} */}</TCell>
-                <TCell>{String(props[key].required)}</TCell>
-                <TCell>
-                  {do {
-                    if (props[key].description) {
-                      const val = props[key].description;
-                      val === 'DEPRECATED' ? (
-                        <Tag color={Color.ORANGE} inversed>
-                          {val}
-                        </Tag>
-                      ) : (
-                        val
-                      );
-                    } else if (props[key].type.name === 'enum') {
-                      const values = props[key].type.value.map((v) => v.value.replace(/'/g, ''));
-                      `One of: ${values.join(', ')}`;
-                    }
-                  }}
-                </TCell>
-                <TCell>
-                  {/* {do {
-                    if (activeProps) {
-                      <Prop
-                        enums={{
-                          ...enums,
-                          Category,
-                          Size,
-                          Tier,
-                          Align,
-                          Position,
-                          Color,
-                          Shade,
-                        }}
-                        name={key}
-                        prop={props[key]}
-                        value={activeProps[key]}
-                        onChange={onChange}
-                      />;
-                    }
-                  }} */}
-                </TCell>
-              </TRow>
-            ))}
+            .map((key) => {
+              const prop = props[key];
+              
+              return (
+                <TRow key={key}>
+                  {/* Name */}
+                  <TCell>{key}</TCell>
+                  {/* Type */}
+                  <TCell>
+                    {do {
+                      if (_isEnum(prop)) {
+                        <Tooltip
+                          content={<PropsInfo props={prop.type.values} />}
+                          side={Position.RIGHT}>
+                          <Flex justify={FlexJustify.START}>
+                            <FlexItem>{prop.type.name}</FlexItem>
+                            <FlexItem>
+                              <Margin size={{ left: Size.EXTRA_SMALL }}>
+                                <span style={{ color: sv.colorSecondary }}>
+                                  <Icon name="info" />
+                                </span>
+                              </Margin>
+                            </FlexItem>
+                          </Flex>
+                        </Tooltip>;
+                      } else {
+                        prop.type.name;
+                      }
+                    }}
+                  </TCell>
+                  {/* Default */}
+                  <TCell>{/* {prop.defaultValue?.value.replace(/'/g, '') || null} */}</TCell>
+                  {/* Required */}
+                  <TCell>{String(prop.required)}</TCell>
+                  {/* Description */}
+                  <TCell>
+                    {do {
+                      if (_hasDescription(prop)) {
+                        const { description } = prop;
+                        if (description.tag === 'deprecated') {
+                          return (
+                            <React.Fragment>
+                              <Margin size={{ right: Size.EXTRA_SMALL }} style={{ display: 'inline-block'}} >
+                                <Tag color={Color.ORANGE} inversed>
+                                  DEPRECATED
+                                </Tag>
+                              </Margin>
+                              {capitalizeFirst(description.text)}
+                            </React.Fragment>
+                          );
+                        } else {
+                          capitalizeFirst(description.text);
+                        }
+                      } else if (_isEnum(prop)) {
+                        const { values } = prop.type;
+                        `One of: ${values.join(', ')}`;
+                      }
+                    }}
+                  </TCell>
+                  <TCell>
+                    {/* {do {
+                      if (activeProps) {
+                        <Prop
+                          enums={{
+                            ...enums,
+                            Category,
+                            Size,
+                            Tier,
+                            Align,
+                            Position,
+                            Color,
+                            Shade,
+                          }}
+                          name={key}
+                          prop={prop}
+                          value={activeProps[key]}
+                          onChange={onChange}
+                        />;
+                      }
+                    }} */}
+                  </TCell>
+                </TRow>
+              );
+            })
+          }
         </TBody>
       </Table>
     </div>
