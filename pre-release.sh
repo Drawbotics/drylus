f61842b1b3eec8e96567b36495299bcf7a38eb7a
nicmosc <nicmosc@users.noreply.github.com>
2020-03-23 09:14:49 +0100

    change release workflow

--- pre-release.sh
+++ pre-release.sh
@@ -4,4 +4,10 @@ export PATH=$(npm bin):$PATH
 
 npx lerna version prerelease --no-commit-hooks --no-changelog --no-push --yes
 
-npx lerna publish --ignore-prepublish --dist-tag beta
+npx lerna publish --from-package --ignore-prepublish --dist-tag beta --yes
+
+git stash
+
+git push
+
+git push --tags
