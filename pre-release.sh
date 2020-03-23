0c9f296895040ece4be61cf6ccbe8d6aed0a7a00
nicmosc <nicmosc@users.noreply.github.com>
2020-03-23 08:28:54 +0100

    change pre-release script

--- pre-release.sh
+++ pre-release.sh
@@ -2,10 +2,6 @@
 
 export PATH=$(npm bin):$PATH
 
-npx lerna version prerelease --no-commit-hooks --no-changelog --no-git-tag-version --no-push
-
-git add .
-
-git commit -m "bump pre-release version"
+npx lerna version prerelease --no-commit-hooks --no-changelog --no-push --yes
 
 npx lerna publish from-package --ignore-prepublish --no-git-reset --pre-dist-tag beta
