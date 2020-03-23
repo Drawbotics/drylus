7444689a8f9a61fd23a7387810df2265b19d7fbf
nicmosc <nicmosc@users.noreply.github.com>
2020-03-23 08:33:10 +0100

    add yes option to lerna publish

--- pre-release.sh
+++ pre-release.sh
@@ -4,4 +4,4 @@ export PATH=$(npm bin):$PATH
 
 npx lerna version prerelease --no-commit-hooks --no-changelog --no-push --yes
 
-npx lerna publish from-package --ignore-prepublish --no-git-reset --dist-tag beta
+npx lerna publish from-package --ignore-prepublish --no-git-reset --dist-tag beta --yes
