5e4b2827d5e0c71f3b905f19b063c688105e709a
nicmosc <nicmosc@users.noreply.github.com>
2020-03-23 09:07:50 +0100

    remove yes option

--- pre-release.sh
+++ pre-release.sh
@@ -4,4 +4,4 @@ export PATH=$(npm bin):$PATH
 
 npx lerna version prerelease --no-commit-hooks --no-changelog --no-push --yes
 
-npx lerna publish --ignore-prepublish --dist-tag beta --yes
+npx lerna publish --ignore-prepublish --dist-tag beta
