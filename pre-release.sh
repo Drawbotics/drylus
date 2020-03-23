7975f102ca6496cfda29c206629b3daacda3c5ea
nicmosc <nicmosc@users.noreply.github.com>
2020-03-23 10:26:02 +0100

    fix release scrit

--- pre-release.sh
+++ pre-release.sh
@@ -7,7 +7,3 @@ npx lerna version prerelease --no-commit-hooks --no-changelog --yes
 npx lerna publish from-package --ignore-prepublish --dist-tag beta --yes
 
 git stash
-
-git push
-
-git push --tags
