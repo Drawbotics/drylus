19a7928a4816a35da1e4228a800f946daa74a85f
nicmosc <nicmosc@users.noreply.github.com>
2020-04-03 12:22:52 +0200

    try new method

--- pre-release.sh
+++ pre-release.sh
@@ -4,6 +4,6 @@ export PATH=$(npm bin):$PATH
 
 npx lerna version prerelease --no-commit-hooks --no-changelog --yes
 
-npx lerna publish from-package --ignore-prepublish --dist-tag beta --yes
+npx lerna publish from-package --ignore-prepublish --dist-tag beta --tag beta --yes
 
 git stash
