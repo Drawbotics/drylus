b2f163b68427fd9d37b60cfcf45a413a87f3238a
LeopoldArkham <leopold.arkham@gmail.com>
2020-04-08 18:59:18 +0200

    dd missing enum tag

--- packages/react-drylus/src/forms/Select.tsx
+++ packages/react-drylus/src/forms/Select.tsx
@@ -172,6 +172,7 @@ export interface SelectProps<T> {
   /**
    * Size of the select. Can be small or default
    * @default Size.DEFAULT
+   * @description uses enum Size
    */
   size?: Size.SMALL | Size.DEFAULT;
 
