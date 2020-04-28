00e62c074ee65c0f0e434c02fc46bd311841b379
nicmosc <nicmosc@users.noreply.github.com>
2020-04-28 13:07:18 +0200

    fix invisible logo in styleguide mobile

--- packages/styleguide/app/app.js
+++ packages/styleguide/app/app.js
@@ -3,6 +3,7 @@ import '@drawbotics/vanilla-drylus/dist/drylus.css';
 import { DrylusProvider, Layout, Page, Position } from '@drawbotics/react-drylus';
 import { useScreenSize } from '@drawbotics/use-screen-size';
 import drylus from '@drawbotics/vanilla-drylus/dist/drylus.js';
+import { css } from 'emotion';
 import React from 'react';
 import { hot } from 'react-hot-loader';
 import { BrowserRouter } from 'react-router-dom';
@@ -13,34 +14,44 @@ import Root from './routes/Root';
 
 window._drylus = drylus;
 
+const styles = {
+  app: css`
+    [data-element='layout-bar'] {
+      z-index: 10;
+    }
+  `,
+};
+
 const App = () => {
   const { screenSize, ScreenSizes } = useScreenSize();
   return (
-    <BrowserRouter basename="drylus">
-      <DrylusProvider>
-        <Page>
-          <Layout
-            bar={<Sidebar />}
-            position={Position.LEFT}
-            fixed
-            responsive={{
-              L: {
-                bar: <MobileSidebar />,
-                position: Position.TOP,
-                fixed: false,
-              },
-            }}>
-            <div
-              style={{
-                height: '100%',
-                width: screenSize <= ScreenSizes.L ? '100vw' : null,
+    <div className={styles.app}>
+      <BrowserRouter basename="drylus">
+        <DrylusProvider>
+          <Page>
+            <Layout
+              bar={<Sidebar />}
+              position={Position.LEFT}
+              fixed
+              responsive={{
+                L: {
+                  bar: <MobileSidebar />,
+                  position: Position.TOP,
+                  fixed: false,
+                },
               }}>
-              <Root />
-            </div>
-          </Layout>
-        </Page>
-      </DrylusProvider>
-    </BrowserRouter>
+              <div
+                style={{
+                  height: '100%',
+                  width: screenSize <= ScreenSizes.L ? '100vw' : null,
+                }}>
+                <Root />
+              </div>
+            </Layout>
+          </Page>
+        </DrylusProvider>
+      </BrowserRouter>
+    </div>
   );
 };
 
