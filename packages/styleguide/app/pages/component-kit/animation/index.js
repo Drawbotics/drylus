4d1b7c9d1c59fbf1384f31405e427e050d11d3a4
nicmosc <nicmosc@users.noreply.github.com>
2020-05-06 09:10:08 +0200

    get started with route animation

--- packages/styleguide/app/pages/component-kit/animation/index.js
+++ packages/styleguide/app/pages/component-kit/animation/index.js
@@ -1,11 +1,13 @@
 import dashboardGrid from './dashboard-grid';
 import index from './index.mdx';
 import toolbar from './toolbar';
+import routeChanges from './route-changes';
 
 const components = {
   index,
   dashboardGrid,
   toolbar,
+  routeChanges,
 };
 
 export default Object.keys(components)
