2511452ccc3736dc10aff7a3a6401ba6b6625d79
nicmosc <nicmosc@users.noreply.github.com>
2020-10-28 15:48:56 +0100

    remove progress

--- packages/icons/scripts/transform-icons.js
+++ packages/icons/scripts/transform-icons.js
@@ -3,12 +3,6 @@ const fs = require('fs');
 const rimraf = require('rimraf');
 const svgFixer = require('oslllo-svg-fixer');
 
-function printProgress(progress, extra) {
-  process.stdout.clearLine();
-  process.stdout.cursorTo(0);
-  process.stdout.write(progress + '% | ' + extra);
-}
-
 function fixDimensions(string) {
   return string.replace(/width="\d+" height="\d+"/, 'width="100%" height="100%"');
 }
@@ -24,13 +18,10 @@ async function transform(iconsFolder, cacheFolder) {
 
   const iconFiles = fs.readdirSync(cacheFolder);
 
-  let i = 0;
   for (let filename of iconFiles) {
-    printProgress(i / iconFiles.length, 'Processing ' + filename);
     const contents = fs.readFileSync(path.resolve(cacheFolder, filename), 'utf8');
     const fixed = fixDimensions(contents);
     fs.writeFileSync(path.resolve(cacheFolder, filename), fixed);
-    i++;
   }
 }
 
