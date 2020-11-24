7c445fb851c35b1b8c9afe77d264247764b3269c
nicmosc <nick@drawbotics.com>
2020-11-24 10:20:03 +0000

    Deploying to gh-pages from  @ 34f92d329a1adf53fee736e05086c3a2a0b4307e 🚀

---
+++ styleguide/app/app.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="react" />
+import '@drawbotics/vanilla-drylus/dist/drylus.css';
+declare const _default: () => JSX.Element;
+export default _default;
+//# sourceMappingURL=app.d.ts.map
---
+++ styleguide/app/routes/Root.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="react" />
+export declare const Root: () => JSX.Element;
+//# sourceMappingURL=Root.d.ts.map
