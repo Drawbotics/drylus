082dac08618e2380c5abc5881013743abc562d57
nicmosc <nicmosc@users.noreply.github.com>
2021-03-11 11:56:52 +0100

    SegmentedControl: use updated API

--- packages/react-drylus/src/components/SegmentedControl.tsx
+++ packages/react-drylus/src/components/SegmentedControl.tsx
@@ -1,10 +1,11 @@
 import sv from '@drawbotics/drylus-style-vars';
 import { css, cx } from 'emotion';
-import React from 'react';
+import React, { ReactNode } from 'react';
 
 import { Color, Size } from '../enums';
-import { Option, Style } from '../types';
-import { run } from '../utils';
+import { Padding } from '../layout';
+import { Option, Responsive, Style } from '../types';
+import { checkComponentProps, run, useResponsiveProps } from '../utils';
 import { Badge } from './Badge';
 import { Spinner } from './Spinner';
 
@@ -80,6 +81,65 @@ const styles = {
   `,
 };
 
+interface SegmentedControlItemProps {
+  /** Text displayed in the item */
+  text: string;
+
+  /** If true, the item is set as active */
+  active?: boolean;
+
+  /** If true, the item is not clickable */
+  disabled?: boolean;
+
+  /** Triggered when the option is clicked */
+  onClick?: VoidFunction;
+
+  /** Component will be displayed on the right side of the item, works best with Spinner or Badge */
+  trailing?: ReactNode;
+
+  /** Component will be displayed on the left side of the item, works best with Spinner or Badge */
+  leading?: ReactNode;
+
+  /** Used for style overrides */
+  style?: Style;
+
+  /** Used for style overrides */
+  className?: string;
+
+  /** Reponsive prop overrides */
+  responsive?: Responsive<this>;
+}
+
+export const SegmentedControlItem = ({ responsive, ...rest }: SegmentedControlItemProps) => {
+  const {
+    text,
+    disabled,
+    onClick,
+    trailing,
+    leading,
+    active,
+    style,
+    className,
+  } = useResponsiveProps<SegmentedControlItemProps>(rest, responsive);
+  return (
+    <div
+      style={style}
+      className={cx(
+        styles.control,
+        {
+          [styles.active]: active,
+          [styles.disabled]: disabled,
+        },
+        className,
+      )}
+      onClick={disabled ? undefined : onClick}>
+      {leading ? <Padding size={{ right: Size.EXTRA_SMALL }}>{leading}</Padding> : null}
+      <span>{text}</span>
+      {trailing ? <Padding size={{ left: Size.EXTRA_SMALL }}>{trailing}</Padding> : null}
+    </div>
+  );
+};
+
 export interface SegmentedControlOption<T> extends Option<T> {
   /** If given, the control shows a value in a Badge */
   bullet?: number;
@@ -92,57 +152,82 @@ export interface SegmentedControlOption<T> extends Option<T> {
 }
 
 export interface SegmentedControlProps<T> {
-  /** Determines the controls which will be rendered */
+  /**
+   * @deprecated Use SegmentedControl components instead
+   * Determines the controls which will be rendered
+   */
   options: Array<SegmentedControlOption<T>>;
 
-  /** Determines which value is currently active */
+  /**
+   * @deprecated Use SegmentedControl components instead
+   * Determines which value is currently active
+   */
   value: SegmentedControlOption<T>['value'];
 
-  /** Triggered when a control is clicked */
+  /**
+   * @deprecated Use SegmentedControl components instead
+   * Triggered when a control is clicked
+   */
   onChange?: (value: SegmentedControlOption<T>['value']) => void;
+
+  /** Should be SegmentedControlItem */
+  children: ReactNode;
 
   /** Used for style overrides */
   style?: Style;
 
   /** Used for style overrides */
   className?: string;
+
+  /** Reponsive prop overrides */
+  responsive?: Responsive<this>;
 }
 
 export const SegmentedControl = <T extends any>({
+  responsive,
+  options,
   value,
   onChange,
-  options,
-  style,
-  className,
+  ...rest
 }: SegmentedControlProps<T>) => {
+  const { children, style, className } = useResponsiveProps<
+    Omit<SegmentedControlProps<T>, 'options' | 'onChange' | 'value'>
+  >(rest, responsive);
+  checkComponentProps({ children }, { children: [SegmentedControlItem] });
+
   return (
     <div style={style} className={cx(styles.root, className)}>
-      {options.map((option) => (
-        <div
-          key={option.value}
-          className={cx(styles.control, {
-            [styles.active]: value === option.value,
-            [styles.disabled]: option.disabled === true,
-          })}
-          onClick={!option.disabled && onChange != null ? () => onChange(option.value) : undefined}>
-          <span>{option.label}</span>
-          {run(() => {
-            if (option.loading === true) {
-              return (
-                <div data-element="extra" className={styles.extra}>
-                  <Spinner size={Size.SMALL} color={Color.BRAND} />
-                </div>
-              );
-            } else if (option.bullet != null) {
-              return (
-                <div data-element="extra" className={styles.extra}>
-                  <Badge color={Color.BRAND} value={option.bullet} max={99} />
-                </div>
-              );
-            }
-          })}
-        </div>
-      ))}
+      {options == null || children != null
+        ? children
+        : // If using deprecated API, still render until removed in next major version
+          options.map((option) => (
+            <div
+              key={option.value}
+              className={cx(styles.control, {
+                [styles.active]: value === option.value,
+                [styles.disabled]: option.disabled === true,
+              })}
+              onClick={
+                !option.disabled && onChange != null ? () => onChange(option.value) : undefined
+              }>
+              <span>{option.label}</span>
+              {run(() => {
+                if (option.loading === true) {
+                  return (
+                    <div data-element="extra" className={styles.extra}>
+                      <Spinner size={Size.SMALL} color={Color.BRAND} />
+                    </div>
+                  );
+                } else if (option.bullet != null) {
+                  return (
+                    <div data-element="extra" className={styles.extra}>
+                      <Badge color={Color.BRAND} value={option.bullet} max={99} />
+                    </div>
+                  );
+                }
+              })}
+            </div>
+          ))}
     </div>
   );
 };
