41b6b71cf0bd0189e40adfd2c9b594513c24c620
nicmosc <nicmosc@users.noreply.github.com>
2021-09-02 11:38:44 +0200

    Fix: invalidate cloudfront when uploading icons to dev

--- packages/icons/scripts/sync.js
+++ packages/icons/scripts/sync.js
@@ -1,17 +1,42 @@
 const path = require('path');
 const s3sync = require('@drawbotics/s3sync');
+const aws = require('aws-sdk');
 
 const version = require('../package.json').version;
+const cloudfront = new aws.CloudFront();
 
-function sync() {
+function invalidateDistribution(folder) {
+  const params = {
+    DistributionId: 'E2S7MPSAFSXZ32',
+    InvalidationBatch: {
+      CallerReference: `${Math.floor(new Date().getTime() / 1000)}`,
+      Paths: {
+        Quantity: 1,
+        Items: [ `/${folder}*` ],
+      },
+    },
+  };
+  return new Promise((resolve, reject) => {
+    cloudfront.createInvalidation(params, (err, data) => err ? reject(err) : resolve(data));
+  });
+}
+
+async function sync() {
   const isDev = process.env.NODE_ENV !== 'production';
-  s3sync({
+  const folder = 'drycons';
+
+  await s3sync({
     sourceDir: path.resolve(__dirname, '../dist'),
-    folder: 'drycons',
+    folder,
     version,
     dev: isDev,
     forceSync: true,
   });
+
+  if (isDev) {
+    console.log('Uploading to dev, invalidating cdn distribution...');
+    invalidateDistribution(folder + '/dev');
+  }
 }
 
 sync();
