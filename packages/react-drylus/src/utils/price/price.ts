5d98e35204704d9de41b446fed5b33e939a161b4
nicmosc <nicmosc@users.noreply.github.com>
2021-11-22 11:23:07 +0100

    formatPrice: default currency to EUR

--- packages/react-drylus/src/utils/price/price.ts
+++ packages/react-drylus/src/utils/price/price.ts
@@ -16,7 +16,7 @@ export function generateDisplayedPrice({
   locale?: string;
   options?: any;
 }) {
-  const { value, currency } = price;
+  const { value, currency = 'EUR' } = price;
 
   const localeRoot = locale.split('-')[0];
 
