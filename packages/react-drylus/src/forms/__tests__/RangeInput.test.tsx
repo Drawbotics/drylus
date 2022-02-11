f0bf1647cf6b2bb5fa80c5b47bca98f9e730ca72
nicmosc <nicmosc@users.noreply.github.com>
2022-02-11 14:06:24 +0100

    remove useless warning test

--- packages/react-drylus/src/forms/__tests__/RangeInput.test.tsx
+++ packages/react-drylus/src/forms/__tests__/RangeInput.test.tsx
@@ -5,14 +5,6 @@ import { RangeInput } from '../RangeInput';
 
 describe('RangeInput', () => {
   describe('Tooltip', () => {
-    it('should display minimum value when max and min are equal', () => {
-      const componentNode = create(
-        <RangeInput step={1} onChange={(x) => x} max={0} min={0} value={0} />,
-      );
-      const tooltipNode = componentNode.root.findAllByProps({ 'data-element': 'tooltip' })[0];
-      expect(tooltipNode.children[0]).toEqual('0');
-    });
-
     it('should display value', () => {
       const componentNode = create(
         <RangeInput step={1} onChange={(x) => x} max={100} min={0} value={13} />,
